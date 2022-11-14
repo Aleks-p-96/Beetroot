68c365398af6f94f0ceee4958145d35c2fb4a301
Aleks-p-96 <sanyapyrch1996@gmail.com>
2022-11-14 14:17:00 +0200

    + functions

---
+++ JS/4/assets/js/script.js
@@ -0,0 +1,96 @@
+'use strict';
+// alert(`Результати прикладів роботи функцій представлені в консолі.
+// Для перевірки роботи функцій з іншими аргументами їх можна викликати через консоль`);
+console.log(`Назви функцій для виклику:
+argumentsQuantity() : кількість переданих аргументів,
+compare(a,b) : порівняння двох чисел,
+factorial(n) : обчислення факторіалу,
+calcArea(a,b) : обчислення площі прямокутника,
+isPerfect(x) : перевірка чи число є досоналим,
+showPerfectFromRange() : Вивід в консоль досконалих чисел
+з діапазону від a до b.`)
+
+//Мінімум
+//1
+
+console.log(`Function declaration:
+function "function-name" (arguments) {
+    fuction body
+}`);
+console.log(`Function expression:
+let "function-name" = function (arguments) {
+    function body
+};`);
+
+//2
+1
+function argumentsQuantity() {
+    return arguments.length;
+}
+console.log(`Результат роботи функції якій передано 5 аргументів: ${argumentsQuantity(1,38,7,'hdh','qwe')}`)
+
+//3
+
+function compare(a,b) {
+    if (a == b) return 0;
+    if (a > b) return 1;
+    return -1;
+}
+console.log(`Результат порівняння чисел -3 і 8 : ${compare(-3,8)}`)  ;
+
+// 4
+
+let factorial = function (n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++ ) {
+        result = result * i;
+        }
+        return result;
+    };
+console.log(`Результат обчислення факторіалу числа 6 : ${factorial(6)}`)  ;
+
+//5
+
+function digitsToNumber(a,b,c) {
+    let result = +`${a}${b}${c}`;
+    return result;
+}
+
+digitsToNumber(3,8,9);
+
+//6
+
+let calcArea = (a, b = a) => {
+    let result = a * b;
+    return result;
+};
+console.log(`Результат обчислення площі прямокутника з довжинами сторін 3 і 7 : ${calcArea(3,7)}`)
+
+//Норма
+
+//1
+
+let isPerfect = (x) => {
+    let divisiorSum = 0;
+    for (let i = (x - 1) ; i > 0; i-- ) {
+        if (x % i == 0) {
+            divisiorSum = divisiorSum + i;
+        } else continue;
+    }
+    if (divisiorSum == x) {
+        return true;
+    } else return false;
+};
+console.log(`Результат перевірки чи число 28 є досконалим : ${isPerfect(28)}`);
+
+//2
+
+function showPerfectFromRange (a,b) {
+    console.log(`Досконалі числа з проміжку від ${a} до ${b} :`)
+    for(let i = a; i <= b; i++) {
+        if (isPerfect(i)) {
+            console.log(i);
+        }
+    }
+}
+showPerfectFromRange(3,999)
---
+++ JS/5/assets/js/script.js
@@ -0,0 +1,205 @@
+'use strict';
+//Мінімум
+//1
+let age = prompt('How old are you?', 26);
+let message = (age == 0 || age > 0 && age < 12) ? console.log('Ви дитина') :
+    (age >= 12 && age < 18) ? console.log('Ви підліток') :
+    (age >=18 && age < 60) ? console.log('Ви дорослий') :
+    (age > 60 ) ? console.log('Ви пеніонер') :
+    !(age) ? console.log('Введені некоректні дані') : console.log('Введені некоректні дані');
+    console.log(age);
+
+//2
+    let usernum = +prompt('Введіть число від 0 до 9');
+    switch (usernum) {
+        case 1:
+            alert('1 !');
+            break;
+        case 2:
+            alert('2 @');
+            break;
+        case 3:
+            alert('3 #');
+            break;
+        case 4:
+            alert('4 $');
+            break;
+        case 5:
+            alert('5 %');
+            break;
+        case 6:
+            alert('6 ^');
+            break;
+        case 7:
+            alert('7 &');
+            break;
+        case 8:
+            alert('8 *');
+            break;
+        case 9:
+            alert('9 (');
+            break;
+        case 0:
+            alert('0 )');
+            break;
+            
+    }
+
+//3
+let sum = 0;
+let n1 = +prompt('Введіть початок числового діапазону', 3);
+let n2 = +prompt('Введіть кінець числового діапазону', 8);
+for (let i = n1; i <= n2; i++) {
+    sum = sum + i;
+}
+console.log(`Сума чисел числового діапазону від ${n1} до ${n2} = ${sum} `);
+
+
+// 4
+let a = +prompt ('Введіть перше число для знаходження НСД', 55);
+let b = +prompt ('Введіть друге число для знаходження НСД', 255);
+if (a === b) {
+    console.log(`Найбільший спільний дільник ${a} і ${a} це ${a}`);
+} else if (a <= b) {
+    for(let i = a; i >= 0; i--) {
+        if ((a % i == 0) && (b % i == 0)) {
+            console.log(`Найбільший спільний дільник ${a} і ${b} це ${i}`);
+            break;
+        }
+    }
+} else if (a > b) {
+    for(let i = a; i >= 0; i--) {
+        if ((a % i == 0) && (b % i == 0)) {
+            console.log(`Найбільший спільний дільник ${a} і ${b} це ${i}`);
+            break;
+        }
+    }
+}
+
+//5
+let p = prompt('Введіть число для пошуку його дільників',44);
+console.log(`Дільники числа: ${p}`)
+for (let i = p ; i > 0; i--) {
+    if (p % i == 0) {
+        console.log(i);
+    }
+}
+
+//Норма
+//1
+let num = +prompt('Введіть число для визначення чи є воно паліндромом', 123);
+let i = num;
+let reversenum = 0;
+while (i) {
+    reversenum = reversenum * 10 + i % 10;
+    i = Math.floor(i / 10);
+}
+if ( num == reversenum) {
+    console.log (`Число ${num} є паліндромом`);
+} else console.log(`Число ${num} не є паліндромом`);
+
+//2
+let amount = +prompt('Введіть суму покупки', 400);
+let price = (amount >= 200 && amount < 300 ) ? +(amount - (amount * 0.03).toFixed(2)).toFixed(2) :
+    (amount >= 300 && amount < 500 ) ? +(amount - (amount * 0.05).toFixed(2)).toFixed(2) :
+    (amount >= 500) ? +(amount - (amount * 0.07).toFixed(2)).toFixed(2) :
+    amount;
+console.log(`Ціна зі знижкою: ${price}`);
+
+//3
+let positive = 0, negative = 0, zeros = 0, even = 0, odd = 0;
+for (let i = 7; i < 10; i++) {
+    let x = +prompt('Введіть число');
+    if (x == 0) zeros++;
+    if (x > 0) positive++;
+    if (x < 0) negative++;
+    if (x % 2 == 0) even++;
+    if (x % 2 != 0) odd++;
+}
+console.log(`Нулів: ${zeros}`);
+console.log(`Додатніх: ${positive}`);
+console.log(`Від'ємних: ${negative}`);
+console.log(`Парних: ${even}`);
+console.log(`Непарних: ${odd}`);
+
+//4
+let x = ' ', dayCounter = 0, currentDay = '';
+while(x) {
+    dayCounter = 1 + dayCounter % 7;
+    switch(dayCounter) {
+        case 1:
+            currentDay = 'Понеділок';
+            break;
+        case 2:
+            currentDay = 'Вівторок';
+            break;
+        case 3:
+            currentDay = 'Середа';
+            break;
+        case 4:
+            currentDay = 'Четвер';
+            break;
+        case 5:
+            currentDay = 'П\'ятниця';
+            break;
+        case 6:
+            currentDay = 'Субота';
+            break;
+        case 7:
+            currentDay = 'Неділя';
+            break;
+    }
+    x = confirm(`${currentDay},хочеш побачити наступний день`);
+}
+
+//Максимум
+//1
+let findNum = 0,
+    minNum = 0,
+    maxNum = 100;
+while (true) {
+    findNum = Math.round((minNum + maxNum) / 2);
+    if (confirm (`Ваше число ${findNum}?`)) {
+        break;
+    } else if (confirm (`Ваше число > ${findNum}`)) {
+        minNum = findNum;
+    } else if (confirm (`Ваше число < ${findNum}`)) {
+        maxNum = findNum;
+    }
+}
+
+//2
+for (let i = 2; i <= 9; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+}
+
+//3
+
+let day = +prompt('Введіть день (1-31)', '30');
+let month =  +prompt('Введіть місяць (1-12)', '08');
+let year = +prompt('Введіть рік', '1996');
+let leapYear = (year % 4 == 0);
+
+if ((day < 1 || day > 31 || month < 1 || month >12) ||
+	(month == 2 && leapYear && day > 28) ||
+	((month == 4 || month == 6 || month == 9 || month == 11)  && day > 30)) {
+    console.log('Введено некоректні дані');
+} else if ((month == 12) && (day == 31) )  {
+    year ++;
+    day = 131
+    month = 1;
+} else if (month == 2 && leapYear && day == 28) {
+    day = 1;
+    month++;
+} else if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 30) {
+    day = 1;
+    month++;
+} else if (day == 31) {
+    day = 1;
+    month++;
+} else day++;
+month = (month < 10) ? `0${month}` : month;
+day = (day < 10) ? `0${day}` : day;
+console.log(`Наступна дата: ${day}.${month}.${year}`);
