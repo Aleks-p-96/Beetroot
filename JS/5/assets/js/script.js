363f3236211e2d5f2029e594572161dfe1ae98c0
Aleks-p-96 <sanyapyrch1996@gmail.com>
2022-11-16 11:52:57 +0200

    + objects

--- JS/5/assets/js/script.js
+++ JS/5/assets/js/script.js
@@ -1,205 +1,228 @@
 'use strict';
-//Мінімум
-//1
-let age = prompt('How old are you?', 26);
-let message = (age == 0 || age > 0 && age < 12) ? console.log('Ви дитина') :
-    (age >= 12 && age < 18) ? console.log('Ви підліток') :
-    (age >=18 && age < 60) ? console.log('Ви дорослий') :
-    (age > 60 ) ? console.log('Ви пеніонер') :
-    !(age) ? console.log('Введені некоректні дані') : console.log('Введені некоректні дані');
-    console.log(age);
+
+// alert("Всі методи об'єктів ви можете побачити відкривши консоль");
+console.log(`List of objects and methods:
+Object car:
+car.showInfo()
+car.changeDriver()
+car.checkDriver()
+car.calcTimeAndFuel ()
+
+Object time:
+time.showTime ()
+time.changeHours() 
+    enter value in brackets or prompt by default
+time.changeMinutes()
+    enter value in brackets or prompt by default
+time.changeSeconds()
+    enter value in brackets or prompt by default
+
+Object fractionMath:
+fractionMath.add(fractObj1,fractObj2):
+    addition of 2 objects-fractions.
+fractionMath.sub(fractObj1,fractObj2):
+    subtraction of 2 objects-fractions.
+fractionMath.mult(fractObj1,fractObj2):
+    multiplication of 2 objects-fractions.
+fractionMath.div(fractObj1,fractObj2):
+    division of 2 objects-fractions.
+fractionMath.red(fractObj1,fractObj2):
+    reduction of an object-fraction.
+
+Now already created 2 objects of fractions:
+    firstFraction and secondFraction
+You can create new fraction objects using syntax:
+    let name = new Fraction (topNum,bottomNum)
+All created fraction object has method to show them:
+    as example :
+    fract1.showFraction
+
+You can do math operations with created fraction using object fractionMath
+`)
+
+// Мінімум
+// 1
+let car = {
+    manufacturer: 'Daewoo',
+    model: 'Lanos',
+    year: 2008,
+    'average speed': 110,
+    fuelTank: 48,
+    fuelConsumption: 8.5,
+
+    showInfo() {
+        let info = ``;
+        for (let key in this) {
+            if (!(typeof this[key] == 'function')) {
+                info += `
+                        ${key} : ${car[key]}`;
+            }
+        }
+        alert(info);
+    },
+
+    changeDriver(driverName = prompt('Enter the new driver', 'Alex')) {
+        this['driver'] = driverName;
+    },
+
+    checkDriver(driverName = prompt('Enter the driver for check', 'Alex')) {
+        let currentDriver = this['driver'];
+        let checkedDriver = driverName;
+        if (checkedDriver.toUpperCase == currentDriver.toUpperCase) {
+            alert(`Yes, the current driver of this car is ${this['driver']}`);
+            return true;
+        }
+        alert(`No, this car has other driver`);
+        return false;
+
+    },
+
+    calcTimeAndFuel () {
+        // let distance = a;
+        let distance = +prompt('Please enter travel distance in km', );
+        let hours = 0;
+        let minutes = Math.round(distance / this["average speed"] * 60);
+        // console.log(`Totalminutes: ${minutes}`);
+        if (minutes >= 60) {
+            hours = Math.floor(minutes / 60);
+            minutes -= hours * 60;
+        }
+        let addHours = 0;
+        for (let i = 1; i <= hours; i++) {
+            if (i % 4 == 0) {
+                addHours++;
+            }
+        }
+        hours += addHours;
+        let fuel = +(this['fuelConsumption'] / 100 * distance).toFixed(1);
+        alert(`You need ${hours} hours and ${minutes} minutes and ${fuel} liters of fuel to cover this distance`);
+        // console.log(`hours: ${hours}`);
+        // console.log(`minutes: ${minutes}`);
+    },
+}
+// car.showInfo();
+car.changeDriver();
+// car.checkDriver();
+// car.calcTimeAndFuel();
+// for (let key in car) {
+    
+//     console.log(`${key} : ${car[key]}, ${typeof car[key]}`);
+// }
+
 
 //2
-    let usernum = +prompt('Введіть число від 0 до 9');
-    switch (usernum) {
-        case 1:
-            alert('1 !');
-            break;
-        case 2:
-            alert('2 @');
-            break;
-        case 3:
-            alert('3 #');
-            break;
-        case 4:
-            alert('4 $');
-            break;
-        case 5:
-            alert('5 %');
-            break;
-        case 6:
-            alert('6 ^');
-            break;
-        case 7:
-            alert('7 &');
-            break;
-        case 8:
-            alert('8 *');
-            break;
-        case 9:
-            alert('9 (');
-            break;
-        case 0:
-            alert('0 )');
-            break;
-            
-    }
+
+let time = {
+    hours : 3,
+    minutes: 22,
+    seconds: 43,
+
+    showTime () {
+        alert(`Current time is ${this.hours}h ${this.minutes}m ${this.seconds}s`);
+    },
+
+    changeHours (entryValue = +prompt('Enter value to + or - hours', 2)) {
+        this.hours = this.hours + entryValue;
+        if (this.hours < 0) {
+            this.hours = 0;
+            this.minutes = 0;
+            this.seconds = 0;
+        }
+    },
+
+    changeMinutes (entryValue = +prompt('Enter value to + or - minutes', 15)) {
+        let hoursChange;
+        this.minutes = this.minutes + entryValue;
+        if (this.minutes >= 60 || this.minutes < 0 ) {
+            let minutes = this.minutes;
+            hoursChange = Math.floor(minutes / 60);
+            this.minutes -= hoursChange * 60;
+            this.changeHours(hoursChange);
+        }
+    },
+
+    changeSeconds (entryValue = +prompt('Enter value to + or - seconds', 15)) {
+        let minutesChange;
+        this.seconds = this.seconds + entryValue;
+        if (this.seconds >= 60 || this.seconds < 0 ) {
+            let seconds= this.seconds;
+            minutesChange = Math.floor(seconds / 60);
+            this.seconds -= minutesChange * 60;
+            this.changeMinutes(minutesChange);
+        }
+    },
+    
+}
+time.showTime();
+// time.changeMinutes(37);
+time.changeSeconds();
+time.showTime();
+
 
 //3
-let sum = 0;
-let n1 = +prompt('Введіть початок числового діапазону', 3);
-let n2 = +prompt('Введіть кінець числового діапазону', 8);
-for (let i = n1; i <= n2; i++) {
-    sum = sum + i;
+
+function Fraction  (topNumber = +prompt('Please enter top number of Frection', 3),
+                    bottomNumber = +prompt('Please enter bottom number of Frection', 9)) {
+    this['topNumber'] = topNumber;
+    this['bottomNumber'] = bottomNumber;
+
+    this.showFraction = function() {
+        console.log(`${this.topNumber} / ${this.bottomNumber}`)
+    }
 }
-console.log(`Сума чисел числового діапазону від ${n1} до ${n2} = ${sum} `);
+let fract1 = new Fraction ();
+let fract2= new Fraction (5,6);
 
+let fractionMath = {
+    addition(fraction1, fraction2) {
+        this.topNumber = (fraction1['topNumber'] * fraction2['bottomNumber']) +
+                          fraction2['topNumber'] * fraction1['bottomNumber'] ;
+        this.bottomNumber = fraction1['bottomNumber'] * fraction2['bottomNumber'];
+        console.log(`${this.topNumber} / ${this.bottomNumber}`);
+    },
+    subtraction(fraction1, fraction2) {
+        this.topNumber = (fraction1['topNumber'] * fraction2['bottomNumber']) -
+                          fraction2['topNumber'] * fraction1['bottomNumber'] ;
+        this.bottomNumber = fraction1['bottomNumber'] * fraction2['bottomNumber'];
+        console.log(`${this.topNumber} / ${this.bottomNumber}`);
+    },
+    multiplication(fraction1, fraction2) {
+        this.topNumber = fraction1['topNumber'] * fraction2['topNumber'];
+        this.bottomNumber = fraction1['bottomNumber'] * fraction2['bottomNumber'];
+        console.log(`${this.topNumber} / ${this.bottomNumber}`);
+    },
+    division(fraction1, fraction2) {
+        this.topNumber = fraction1['topNumber'] * fraction2['bottomNumber'];
+        this.bottomNumber = fraction1['bottomNumber'] * fraction2['topNumber'];
+        console.log(`${this.topNumber} / ${this.bottomNumber}`);
+    },
+    reduction(fraction) {
+        let divisior = gcd(fraction['topNumber'], fraction['bottomNumber']);
+        this.topNumber = fraction['topNumber'] / divisior;
+        this.bottomNumber = fraction['bottomNumber'] / divisior;
+        console.log(`${this.topNumber} / ${this.bottomNumber}`);
+    },
+}
 
-// 4
-let a = +prompt ('Введіть перше число для знаходження НСД', 55);
-let b = +prompt ('Введіть друге число для знаходження НСД', 255);
-if (a === b) {
-    console.log(`Найбільший спільний дільник ${a} і ${a} це ${a}`);
-} else if (a <= b) {
-    for(let i = a; i >= 0; i--) {
-        if ((a % i == 0) && (b % i == 0)) {
-            console.log(`Найбільший спільний дільник ${a} і ${b} це ${i}`);
-            break;
+function gcd (num1,num2) { //Greatest Common Divisior
+    let i;
+    if (num1 > num2) {
+        i = num2;
+    } else {
+        i = num1;
+    };
+    for (i; i > 0; i-- ) {
+        if ((num1 % i == 0) && (num2 % i == 0)) {
+            return i;
         }
     }
-} else if (a > b) {
-    for(let i = a; i >= 0; i--) {
-        if ((a % i == 0) && (b % i == 0)) {
-            console.log(`Найбільший спільний дільник ${a} і ${b} це ${i}`);
-            break;
-        }
-    }
 }
 
-//5
-let p = prompt('Введіть число для пошуку його дільників',44);
-console.log(`Дільники числа: ${p}`)
-for (let i = p ; i > 0; i--) {
-    if (p % i == 0) {
-        console.log(i);
-    }
-}
+fract1.showFraction();
+fract2.showFraction();
+// fractionMath.reduction(fract1);
 
-//Норма
-//1
-let num = +prompt('Введіть число для визначення чи є воно паліндромом', 123);
-let i = num;
-let reversenum = 0;
-while (i) {
-    reversenum = reversenum * 10 + i % 10;
-    i = Math.floor(i / 10);
-}
-if ( num == reversenum) {
-    console.log (`Число ${num} є паліндромом`);
-} else console.log(`Число ${num} не є паліндромом`);
 
-//2
-let amount = +prompt('Введіть суму покупки', 400);
-let price = (amount >= 200 && amount < 300 ) ? +(amount - (amount * 0.03).toFixed(2)).toFixed(2) :
-    (amount >= 300 && amount < 500 ) ? +(amount - (amount * 0.05).toFixed(2)).toFixed(2) :
-    (amount >= 500) ? +(amount - (amount * 0.07).toFixed(2)).toFixed(2) :
-    amount;
-console.log(`Ціна зі знижкою: ${price}`);
 
-//3
-let positive = 0, negative = 0, zeros = 0, even = 0, odd = 0;
-for (let i = 7; i < 10; i++) {
-    let x = +prompt('Введіть число');
-    if (x == 0) zeros++;
-    if (x > 0) positive++;
-    if (x < 0) negative++;
-    if (x % 2 == 0) even++;
-    if (x % 2 != 0) odd++;
-}
-console.log(`Нулів: ${zeros}`);
-console.log(`Додатніх: ${positive}`);
-console.log(`Від'ємних: ${negative}`);
-console.log(`Парних: ${even}`);
-console.log(`Непарних: ${odd}`);
 
-//4
-let x = ' ', dayCounter = 0, currentDay = '';
-while(x) {
-    dayCounter = 1 + dayCounter % 7;
-    switch(dayCounter) {
-        case 1:
-            currentDay = 'Понеділок';
-            break;
-        case 2:
-            currentDay = 'Вівторок';
-            break;
-        case 3:
-            currentDay = 'Середа';
-            break;
-        case 4:
-            currentDay = 'Четвер';
-            break;
-        case 5:
-            currentDay = 'П\'ятниця';
-            break;
-        case 6:
-            currentDay = 'Субота';
-            break;
-        case 7:
-            currentDay = 'Неділя';
-            break;
-    }
-    x = confirm(`${currentDay},хочеш побачити наступний день`);
-}
 
-//Максимум
-//1
-let findNum = 0,
-    minNum = 0,
-    maxNum = 100;
-while (true) {
-    findNum = Math.round((minNum + maxNum) / 2);
-    if (confirm (`Ваше число ${findNum}?`)) {
-        break;
-    } else if (confirm (`Ваше число > ${findNum}`)) {
-        minNum = findNum;
-    } else if (confirm (`Ваше число < ${findNum}`)) {
-        maxNum = findNum;
-    }
-}
-
-//2
-for (let i = 2; i <= 9; i++) {
-    for (let j = 1; j <= 10; j++) {
-        console.log(`${i} * ${j} = ${i * j}`);
-    }
-}
-
-//3
-
-let day = +prompt('Введіть день (1-31)', '30');
-let month =  +prompt('Введіть місяць (1-12)', '08');
-let year = +prompt('Введіть рік', '1996');
-let leapYear = (year % 4 == 0);
-
-if ((day < 1 || day > 31 || month < 1 || month >12) ||
-	(month == 2 && leapYear && day > 28) ||
-	((month == 4 || month == 6 || month == 9 || month == 11)  && day > 30)) {
-    console.log('Введено некоректні дані');
-} else if ((month == 12) && (day == 31) )  {
-    year ++;
-    day = 131
-    month = 1;
-} else if (month == 2 && leapYear && day == 28) {
-    day = 1;
-    month++;
-} else if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 30) {
-    day = 1;
-    month++;
-} else if (day == 31) {
-    day = 1;
-    month++;
-} else day++;
-month = (month < 10) ? `0${month}` : month;
-day = (day < 10) ? `0${day}` : day;
-console.log(`Наступна дата: ${day}.${month}.${year}`);
